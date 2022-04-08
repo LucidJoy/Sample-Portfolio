2e95c0d837543931a00b3b51b95517b0e60771cd
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-04-08 19:12:29 +0530

    added pacman loading animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,6 +9,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import Loader from 'react-loaders';
 
 import './About.scss';
 
@@ -22,62 +23,66 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
 
-        <p>
-          I'm very ambitious front-end developer looking for a role in
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
+          <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
 
-        <p>
-          I'm quietly confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
-        </p>
+          <p>
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
 
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
-      </div>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
-          </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
 
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
-          </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
 
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
-          </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
 
-          <div className="face4">
-            <FontAwesomeIcon icon={faNodeJs} color="#339933" />
-          </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faNodeJs} color="#339933" />
+            </div>
 
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
-          </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
 
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Loader type="pacman" />
+    </>
   );
 };
 
